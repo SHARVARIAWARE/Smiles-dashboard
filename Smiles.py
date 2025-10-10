7ae46b96498e329b04b4bf9e6570a2f77bf3e695
SHARVARIAWARE <161229921+SHARVARIAWARE@users.noreply.github.com>
2025-10-10 16:24:46 +0530

    Update Smiles.py

--- Smiles.py
+++ Smiles.py
@@ -38,11 +38,10 @@ def load_models():
     try:
         return {
             "Activity": joblib.load("hybrid_model.pkl"),
-            "(Absorption) Caco-2": joblib.load("absorp_model.pkl"),
-            "(Distribution) BBB": joblib.load("distri_model.pkl"),
-            "(Metabolism) CYP3A4": joblib.load("meta_model.pkl"),
-            "(Excretion) Obach": joblib.load("exc_model.pkl"),
-            "(Toxicity) NR-AR": joblib.load("toxicity_model.pkl"),
+            "(Absorption) Caco-2": joblib.load("absorption_hybrid_Caco2_Wang.pkl"),
+            
+            "(Metabolism) CYP2D6 Substrate": joblib.load("Metabolism_Hybrid_CYP2D6.pkl"),
+           
         }
     except FileNotFoundError as e:
         st.error(f"❌ Model file not found: {e}")
@@ -119,8 +118,8 @@ if uploaded_file:
                     df[name] = model.predict(df[descriptor_columns])
 
             # Clean and re-map values
-            df["(Metabolism) CYP3A4"] = df["(Metabolism) CYP3A4"].map({0: "Non-Metabolic (0)", 1: "Metabolic (1)"})
-            df["(Toxicity) NR-AR"] = df["(Toxicity) NR-AR"].map({0: "Non-Toxic (0)", 1: "Toxic (1)"})
+            df["(Metabolism) CYP2D6 Substrate"] = df["(Metabolism) CYP2D6 Substrate"].map({0: "Non-Metabolic (0)", 1: "Metabolic (1)"})
+            #df["(Toxicity) NR-AR"] = df["(Toxicity) NR-AR"].map({0: "Non-Toxic (0)", 1: "Toxic (1)"})
 
             df.drop(columns=descriptor_columns, inplace=True)
 
